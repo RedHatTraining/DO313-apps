18f5da5248019b38703e6e996e235a844e3eb50f
Rafa Ruiz <rruizher@redhat.com>
2022-11-28 19:20:46 +0100

    Cosmetics in view

--- finance-webapp/src/main/java/com/example/ForbiddenExceptionMapper.java
+++ finance-webapp/src/main/java/com/example/ForbiddenExceptionMapper.java
@@ -13,10 +13,6 @@ import io.quarkus.security.ForbiddenException;
 public class ForbiddenExceptionMapper implements ExceptionMapper<ForbiddenException> {
   @Override
   public Response toResponse(ForbiddenException exception) {   
-    return Response.serverError().entity("{\"message\":\"You are not in the required role\"}").build();
-    //return Response.status(Response.Status.FORBIDDEN)
-      
-    //  .entity(new Exception("You don't have the required role"))
-    //  .build();
+    return Response.serverError().entity("{\"message\":\"You are not in the required role\"}").build();    
   }
 }
